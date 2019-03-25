ae75e3a535cec5bafd1fe75aa21a237084d5cd92
Anna Kalinina <caressyy@gmail.com>
2019-03-25 17:33:37 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,18 @@
+let x = 2;
+let y = 22;
+let z = 0;
+let result = x * y + 5 * z + x - 1;
+console.log(result);
+
+let algebra = 5;
+geography = 4;
+physics = 4;
+
+let midlemark = (algebra + geography + physics) / 3;
+console.log(midlemark);
+
+let name;
+name = "Anna";
+let message = "Привет, мир! Меня зовут " + name;
+console.log(message);
+
